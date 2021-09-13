78424d1393f990e462f8b50faa8f99a9cc9d07e9
Dianne <mdianne.gabriel@gmail.com>
2021-09-13 13:10:30 -0400

    Test player ar answers

--- question.rb
+++ question.rb
@@ -3,13 +3,15 @@ class Question
     number_one = rand(1..20)
     number_two = rand(1..20)
     puts "#{player.name}: What does #{number_one} plus #{number_two} equal?"
+    print "Your answer: "
     player_answer = gets.chomp.to_i
-    correct_answer = eval(number_one + number_two)
+    correct_answer = number_one + number_two
     if player_answer == correct_answer
       puts "#{player.name}: YES! You are correct."
     else
       puts "#{player.name}: Seriously? NO!"
       player.life = player.life - 1
+      puts "#{player.name}: #{player.life}/3"
     end
   end
 end
