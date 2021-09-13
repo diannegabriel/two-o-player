17ab05a0ca03b0164eaff5b66855b407cb192a03
Dianne <mdianne.gabriel@gmail.com>
2021-09-13 11:42:37 -0400

    Modify logic to create Player and show player names

--- game.rb
+++ game.rb
@@ -1,5 +1,5 @@
 def new_game(player_one, player_two)
   puts "Welcome to TwO-O-Player Math Game!"
-  puts "Player One will be: #{player_one}"
-  puts "Player Two will be: #{player_two}"
+  puts "Player One will be: #{player_one.name}"
+  puts "Player Two will be: #{player_two.name}"
 end
--- main.rb
+++ main.rb
@@ -1,5 +1,12 @@
 # Define a main.rb file that will require all the other files
 
 require './game'
+require './player'
 
-new_game("Chubby", "Cutie")
+player_one = Player.new('Chubby')
+player_two = Player.new('Cutie')
+
+# puts player_one.name
+
+# new_game("Chubby", "Cutie")
+new_game(player_one, player_two)
