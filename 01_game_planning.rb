4a899bf2cc7cd8dd4b491cd58b60dfedc1c2480a
Dianne <mdianne.gabriel@gmail.com>
2021-09-13 11:15:35 -0400

    Initialize planning phase

---
+++ 01_game_planning.rb
@@ -0,0 +1,36 @@
+# DETAILS
+# 2 player
+# Starts with 3 lives
+# = P1 3L vs P2 3L
+
+# Pick 2 numbers between 1 and 20
+
+# Lose a life when player gets the question wrong
+# = P1 2L vs P2 3L
+
+# EXAMPLE
+# Player 1: What does 5 plus 3 equal?
+# 9
+# Player 1: Seriously? NO!
+# P1: 2/3 vs P2: 3/3
+# ---- NEW TURN ----
+# Player 2: What does 2 plus 6 equal?
+# 8
+# Player 2: YES! You are correct.
+# P1: 2/3 vs P2: 3/3
+# ---- NEW TURN ----
+# Player 1 wins with a score of 1/3
+# ---- GAME OVER ----
+# Good bye!
+
+# Task 1: Extract Nouns for Classes
+# Player > One
+# Player > Two
+# lives = 3
+# score
+# wrong_answer
+
+# Task 2: Write their roles
+# There will be two players
+# def initialize lives, score
+# def final_score
