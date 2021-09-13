c33566b15dfafcbee916d1722039e1f4fcbef3e2
Dianne <mdianne.gabriel@gmail.com>
2021-09-13 12:07:40 -0400

    Use get.chomp to get user inputs on game

--- game.rb
+++ game.rb
@@ -1,5 +1,11 @@
-def new_game(player_one, player_two)
-  puts "Welcome to TwO-O-Player Math Game!"
-  puts "Player One will be: #{player_one.name}"
-  puts "Player Two will be: #{player_two.name}"
+class Game
+  def initialize
+    puts "Welcome to TwO-O-Player Math Game!"
+    print "Enter your name: "
+    player_one = Player.new(gets.chomp)
+    puts "Player One will be #{player_one.name}."
+    print "Enter your name: "
+    player_two = Player.new(gets.chomp)
+    puts "Player Two will be #{player_two.name}."
+  end 
 end
--- main.rb
+++ main.rb
@@ -3,7 +3,11 @@
 require './game'
 require './player'
 
-player_one = Player.new('Chubby')
-player_two = Player.new('Tobi')
+# player_one = Player.new('Chubby')
+# player_two = Player.new('Tobi')
 
-new_game(player_one, player_two)
+# player_one = Player.new(gets.chomp)
+# player_two = Player.new(gets.chomp)
+
+# new_game(player_one, player_two)
+new_game = Game.new
